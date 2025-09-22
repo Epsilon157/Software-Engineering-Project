90b4fcc2b562eed631cdf9b34a9f5ced7b2e167b
jace-bormann <jabmann5@gmail.com>
2025-09-22 18:19:55 -0500

    evil commit

--- measureDisplay.js
+++ measureDisplay.js
@@ -12,7 +12,9 @@ for (let i=0; i<10; i++){
     document.querySelector('my-button').appendChild(newButton);*/
 
     var x = document.createElement("BUTTON");
+    x.classList.add('measure-button');
     x.textContent = "Button " + i;
+    
     document.body.appendChild(x);
 }
 
