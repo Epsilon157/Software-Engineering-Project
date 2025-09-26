befa934c33f794d0de9ac1f0c28ba3057003506e
jace-bormann <jabmann5@gmail.com>
2025-09-25 22:25:46 -0500

    Update measureDisplay.js

--- measureDisplay.js
+++ measureDisplay.js
@@ -1,19 +1,9 @@
 for (let i=0; i<10; i++){
-    /*
-    newButton = document.createElement('button');
-    newButton.textContent = 'Test Button';
-    newButton.classList.add('my-button'); // Apply existing CSS class
-    newButton.addEventListener('click', () => {
-        console.log('New button clicked!');
-    });
-
-    document.body.appendChild(newButton);
-
-    document.querySelector('my-button').appendChild(newButton);*/
 
     var x = document.createElement("BUTTON");
     x.classList.add('measure-button');
     x.textContent = "Button " + i;
+    x.onclick="window.open('/measure')";
     
     document.getElementById("measure-list").appendChild(x);
 }
