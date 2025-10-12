67ec039855e543ba02384f46cd04de9dbaf6f7f8
jace-bormann <jabmann5@gmail.com>
2025-10-12 18:21:33 -0500

    idk man

---
+++ api/query.js
@@ -0,0 +1,13 @@
+export async function onRequest(context) {
+  // Run a SQL query
+  const { results } = await context.env.DB.prepare(
+    "SELECT people_id FROM legislators WHERE people_id > ?"
+  )
+    .bind(18)  // Use .bind() to safely inject values
+    .all();    // Execute and get all results
+
+  // Return the query result as JSON
+  return new Response(JSON.stringify(results), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
