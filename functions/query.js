3365da8989549444619dd57c54731374e38a1c49
Epsilon157 <205445515+Epsilon157@users.noreply.github.com>
2025-10-14 13:58:26 -0500

    Updating working sql query

--- functions/query.js
+++ functions/query.js
@@ -1,4 +1,4 @@
 export async function onRequestGet({ env }) {
-  const result = await env.DB.prepare("SELECT * FROM legislators").all();
+  const result = await env.DB.prepare("SELECT * FROM votes").all();
   return Response.json(result);
 }
