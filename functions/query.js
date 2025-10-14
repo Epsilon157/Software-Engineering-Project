2c318f23c8ab319195749ce2d24c7a3d3caf41d9
jace-bormann <jabmann5@gmail.com>
2025-10-14 13:36:17 -0500

    Update query.js

--- functions/query.js
+++ functions/query.js
@@ -1,3 +1,4 @@
-export function onRequest(context) {
-  return new Response("Hello, world!");
+export async function onRequestGet({ env }) {
+  const result = await env.DB.prepare("SELECT * FROM legislators").all();
+  return Response.json(result);
 }
