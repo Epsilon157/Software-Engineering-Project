4e4413028d0518e57c7a58299db1900dc83fd3c3
jace-bormann <jabmann5@gmail.com>
2025-10-29 17:03:23 -0500

    cleaning up

--- measurePage.js
+++ measurePage.js
@@ -5,10 +5,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('voteid');
 
-let p = document.getElementById('billID');
-p.textContent = `Hello ${id}`;
-
-async function loadMessages() {
+async function loadMeasurePage() {
     const res = await fetch(`query?vote_id=${id}`);
     const data = await res.json();
 
@@ -17,19 +14,4 @@ async function loadMessages() {
     });
 }
 
-loadMessages();
-
-/*
-async function loadMessages() {
-        const res = await fetch('query');
-        const data = await res.json();
-
-        const list = document.getElementById('messages');
-        (data.results || []).forEach(row => {
-          const li = document.createElement('li');
-          li.textContent = row.desc;
-          list.appendChild(li);
-        });
-        }
-
-        loadMessages();*/
+loadMeasurePage();
