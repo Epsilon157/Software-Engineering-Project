414117c0506545feefd6e52327eb5937660ee6ed
jace-bormann <jabmann5@gmail.com>
2025-10-29 16:55:54 -0500

    Update query.js

--- functions/query.js
+++ functions/query.js
@@ -2,7 +2,14 @@ export async function onRequestGet({ request, env }) {
   const url = new URL(request.url);
   const id = url.searchParams.get("vote_id");
 
-  const result = await env.DB.prepare("SELECT v1.bill_id, v1.desc, v1.date, v1.yea_votes, v1.nay_votes, v1.chamber, m.measure_number, m.measure_type, m.primary_author, json_group_array(json_object('coathor_id', c.people_id, 'name', l.name)) AS coathors FROM votes_1 AS v1 INNER JOIN measures AS m ON v1.bill_id = m.bill_id INNER JOIN coauthors AS c ON v1.bill_id = c.bill_id INNER JOIN legislators AS l ON c.people_id = l.people_id WHERE v1.roll_call_id = ?;")
+  const vote_id_query = `SELECT v1.bill_id, v1.desc, v1.date, v1.yea_votes, v1.nay_votes, v1.chamber, m.measure_number, m.measure_type, m.primary_author, json_group_array(json_object('coathor_id', c.people_id, 'name', l.name)) AS coathors 
+                        FROM votes_1 AS v1 
+                        INNER JOIN measures AS m ON v1.bill_id = m.bill_id 
+                        INNER JOIN coauthors AS c ON v1.bill_id = c.bill_id 
+                        INNER JOIN legislators AS l ON c.people_id = l.people_id 
+                        WHERE v1.roll_call_id = ?;`;
+
+  const result = await env.DB.prepare(vote_id_query)
   .bind(id)
   .all();
   return Response.json(result);
