6d3acc9940d1bbc1b5672f29ab279dfaba9f03c6
jace-bormann <jabmann5@gmail.com>
2025-10-30 20:09:12 -0500

    Update measureDisplay.js

--- measureDisplay.js
+++ measureDisplay.js
@@ -25,7 +25,7 @@ async function loadSearchPage() {
     for(let i=1; i<=20; i++){
         var x = document.createElement("BUTTON");
         x.classList.add('measure-button');
-        x.innerHTML = "(Session): > (Location): > (BillID): <br>Date: " + data[i].date + "<br>Vote: <br>Description: ";
+        x.innerHTML = "(Session): > (Location): > (BillID): <br>Date: " + data.results[i].date + "<br>Vote: <br>Description: ";
 
         x.addEventListener('click', () => {
             const url = new URL('https://soonerview.org/measure');
