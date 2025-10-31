86be39067bc90dcdbd82b8dccf8ce15b4a962fbf
jace-bormann <jabmann5@gmail.com>
2025-10-30 20:03:52 -0500

    this isnt going to work

--- measureDisplay.js
+++ measureDisplay.js
@@ -1,3 +1,4 @@
+/*
 for (let i=1; i<=20; i++){
 
     var x = document.createElement("BUTTON");
@@ -7,11 +8,33 @@ for (let i=1; i<=20; i++){
     x.addEventListener('click', () => {
         const url = new URL('https://soonerview.org/measure');
 
-        url.searchParams.set("voteid", i);
+        url.searchParams.set("vote_id", i);
 
         window.location.href = url.toString();
     });
     
     document.getElementById("measure-list").appendChild(x);
-}
 
+}*/
+
+async function loadSearchPage() {
+    const res = await fetch(`query?vote_id=${id}`);
+    const data = await res.json();
+
+    
+    for(let i=1; i<=20; i++){
+        var x = document.createElement("BUTTON");
+        x.classList.add('measure-button');
+        x.innerHTML = "(Session): > (Location): > (BillID): <br>Date: " + data[i].date + "<br>Vote: <br>Description: ";
+
+        x.addEventListener('click', () => {
+            const url = new URL('https://soonerview.org/measure');
+
+            url.searchParams.set("vote_id", i);
+
+            window.location.href = url.toString();
+        });
+    
+        document.getElementById("measure-list").appendChild(x);
+    }
+}
