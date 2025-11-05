310d4553565eb28b4d42fd466f2096872dd87201
Epsilon157 <205445515+Epsilon157@users.noreply.github.com>
2025-11-05 10:53:30 -0600

    trying a new thing in the query

--- functions/query.js
+++ functions/query.js
@@ -33,11 +33,12 @@ const url = new URL(request.url);
   else if(url.searchParams.has("vote_id")){
     const id = url.searchParams.get("vote_id");
 
-    const vote_id_query = `SELECT v1.bill_id, v1.desc, v1.date, v1.yea_votes, v1.nay_votes, v1.chamber, m.measure_number, m.measure_type, m.primary_author, json_group_array(json_object('coathor_id', c.people_id, 'name', l.name)) AS coauthors 
+    const vote_id_query = `SELECT v1.bill_id, v1.desc, v1.date, v1.yea_votes, v1.nay_votes, v1.chamber, m.measure_number, m.measure_type, m.primary_author, l.name, json_group_array(json_object('coathor_id', c.people_id, 'name', l.name)) AS coauthors 
                           FROM votes_1 AS v1 
                           INNER JOIN measures AS m ON v1.bill_id = m.bill_id 
                           INNER JOIN coauthors AS c ON v1.bill_id = c.bill_id 
                           INNER JOIN legislators AS l ON c.people_id = l.people_id 
+                          INNER JOIN legislators AS l2 ON m.primary_author = people_id
                           WHERE v1.roll_call_id = ?;`;
 
     const result = await env.DB.prepare(vote_id_query)
@@ -46,8 +47,9 @@ const url = new URL(request.url);
     return Response.json(result);
   }
   //If search parameter in url then activate search query
-  //TODO: Allow different parameters to search by. As easy as getting the parameters and updating the query i think
+  //TODO: Allow different parameters to search by. As easy as getting the parameters and updating the query I think
   else if(url.searchParams.has("search")){
+    const searchType = url.searchParams.get("search");
     const search_query = "SELECT * FROM votes_1 ORDER BY date LIMIT 30;";
 
     const result = await env.DB.prepare(search_query)
