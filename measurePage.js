3e1b21383804dd2b1a22fc780c0a381ab5f54fd8
Epsilon157 <205445515+Epsilon157@users.noreply.github.com>
2025-11-10 11:13:59 -0600

    Removed highlight

--- measurePage.js
+++ measurePage.js
@@ -18,15 +18,15 @@ async function loadMeasurePage() {
         //document.getElementById('coauthors').textContent = `Coauthors: ${coauthorsNames}`;// No highlight or bold
         //document.getElementById('coauthors').innerHTML = `<strong>Coauthors</strong>: ${coauthorsNames}`;// Just bold
         //document.getElementById('coauthors').innerHTML = `<mark>Coauthors</mark>: ${coauthorsNames}`;// Just highlight
-        document.getElementById('coauthors').innerHTML = `<strong><mark>Coauthors:\n</mark></strong> ${coauthorsNames}`;// Highlight and bold
+        document.getElementById('coauthors').innerHTML = `<strong><mark>Coauthors:</mark></strong>\n ${coauthorsNames}`;// Highlight and bold
 
         //document.getElementById('coauthors').textContent = `Coauthors: ${row.coauthors.map(coauthors => String(coauthors.name))}`;
         
-        document.getElementById('author').innerHTML = `<strong><mark>Author:\n</mark></strong> ${row.primary_author_name}`;
-        document.getElementById('date').innerHTML = `<strong><mark>Date:\n</mark></strong> ${row.date}`;
-        document.getElementById('desc').innerHTML = `<strong><mark>Description:\n</mark></strong> ${row.desc}`;
-        document.getElementById('yeaheader').innerHTML = `<strong><mark>Yea:\n</mark></strong> ${row.yea_votes}`;
-        document.getElementById('nayheader').innerHTML = `<strong><mark>Nay:\n</mark></strong> ${row.nay_votes}`;
+        document.getElementById('author').innerHTML = `<strong>Author:\n</strong>\n ${row.primary_author_name}`;
+        document.getElementById('date').innerHTML = `<strong>Date:\n</strong>\n ${row.date}`;
+        document.getElementById('desc').innerHTML = `<strong>Description:\n</strong>\n ${row.desc}`;
+        document.getElementById('yeaheader').innerHTML = `<strong>Yea:\n</strong>\n ${row.yea_votes}`;
+        document.getElementById('nayheader').innerHTML = `<strong>Nay:\n</strong>\n ${row.nay_votes}`;
 
         /*
         document.getElementById('author').textContent = `Author: ${row.primary_author_name}`;
