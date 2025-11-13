f5b24ee5e74508de04ac2a3cf222ebc3fe5fe728
jace-bormann <jabmann5@gmail.com>
2025-11-13 14:38:29 -0600

    Update measureDisplay.js

--- measureDisplay.js
+++ measureDisplay.js
@@ -1,4 +1,4 @@
-
+/*
 async function loadSearchPage() {
     const res = await fetch(`query?search`);
     const data = await res.json();
@@ -8,6 +8,33 @@ async function loadSearchPage() {
         const voteResponse = await fetch(`query?vote_id=${data.results[i].roll_call_id}`);
         const voteData = await voteResponse.json();
 
+        var x = document.createElement("BUTTON");
+        x.classList.add('measure-button');
+        x.innerHTML = `2025 Session > ${voteData.results[0].chamber} > ${voteData.results[0].measure_number} <br>Date: ${voteData.results[0].date} <br>Yea: ${voteData.results[0].yea_votes} Nay: ${voteData.results[0].nay_votes} <br>${voteData.results[0].desc}`;
+
+        x.addEventListener('click', () => {
+            const url = new URL('https://soonerview.org/measure');
+
+            url.searchParams.set("vote_id", data.results[i].roll_call_id);
+
+            window.location.href = url.toString();
+        });
+    
+        document.getElementById("measure-list").appendChild(x);
+    }
+}*/
+
+async function loadSearchPage(){
+    const votePromises = [];
+
+    for(let i=0; i<=20; i++){
+        votePromises.push(fetch(`query?vote_id=${i}`));
+    }
+    
+    const voteResponses = await Promise.all(votePromises);
+    for(let i = 0; i < voteResponses.length; i++){
+        const voteData = await voteResponses[i].json();
+        
         var x = document.createElement("BUTTON");
         x.classList.add('measure-button');
         x.innerHTML = `2025 Session > ${voteData.results[0].chamber} > ${voteData.results[0].measure_number} <br>Date: ${voteData.results[0].date} <br>Yea: ${voteData.results[0].yea_votes} Nay: ${voteData.results[0].nay_votes} <br>${voteData.results[0].desc}`;
