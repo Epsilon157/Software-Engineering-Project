7ea412969c031aa3077461de577d6a733ea4b604
Ryan Price <ryanpricesemail@gmail.com>
2025-11-28 23:03:17 -0600

    promptpromptprompt

--- functions/ai-summary/index.js
+++ functions/ai-summary/index.js
@@ -17,7 +17,7 @@ export async function onRequestPost({ request, env }) {
         const messages = [
             {
                 role: 'system',
-                content: `Create a short summary of this legaslative bill and keep it at 1300 characters. Only respond with the summary.`
+                content: `Create a short summary of this legaslative bill and keep it at less than 2000 characters. Do not include any other information but the summary in your reply.'
             },
             {
                 role: 'user',
